f46f566009e085ddbb922d972496ba41460067f1
Elina <zanywstakanelina@gmail.com>
2025-06-10 17:26:20 +0200

    ашчув

--- kaliop-fb/src/scss/vite.config.js
+++ kaliop-fb/src/scss/vite.config.js
@@ -2,25 +2,25 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
-  root: path.resolve(__dirname, 'src'), 
+  root: path.resolve(__dirname, 'src'),
   build: {
-    outDir: path.resolve(__dirname, 'dist'), 
-    sourcemap: true, 
+    outDir: path.resolve(__dirname, 'dist'),
+    sourcemap: true,
   },
   server: {
-    port: 3000, 
-    open: true, 
+    port: 3000,
+    open: true,
   },
   resolve: {
     alias: {
-      '@assets': path.resolve(__dirname, 'src/assets'), 
-      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@assets': path.resolve(__dirname, 'src/scss/assets'),
+      '@styles': path.resolve(__dirname, 'src/scss/styles'),
     },
   },
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "@styles/variables.scss";`,
+        additionalData: `@import "@styles/variable.scss";`,
       },
     },
   },
